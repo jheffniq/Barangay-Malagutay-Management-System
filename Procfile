26f2ba91fcb248e55896943702717ec47b036281
Nic <jheffniq.com>
2022-04-15 19:23:36 +0800

    Prepared for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn software_src.wsgi
--- software_src/settings.py
+++ software_src/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'software_src.urls'
@@ -131,6 +135,7 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = BASE_DIR / "static/images"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -142,3 +147,5 @@ EMAIL_HOST_PASSWORD = '09159204540'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
